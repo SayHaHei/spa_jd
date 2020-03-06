838e4c68c8ee78eb277d5e30190f759997732215
SayHaHei <1041711058@qq.com>
2020-03-06 21:35:34 +0800

    引入全部的cube-ui

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	configureWebpack: {
+    configureWebpack: {
 		devServer: {
 			port: 8080,
       open: true,
@@ -8,5 +8,23 @@ module.exports = {
 
       }
 		}
-	}
+	},
+
+    css: {
+      loaderOptions: {
+        stylus: {
+          'resolve url': true,
+          'import': [
+            './src/theme'
+          ]
+        }
+      }
+    },
+
+    pluginOptions: {
+      'cube-ui': {
+        postCompile: true,
+        theme: true
+      }
+    }
 };
